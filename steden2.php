d6467def0317aef2e5ec4e9b4b2346c7c3f4e27d
Tim Vercammen <83355491+timvercammen@users.noreply.github.com>
2021-12-02 11:13:25 +0100

    oef2.1

--- steden2.php
+++ steden2.php
@@ -40,7 +40,7 @@ $sql = "SELECT * from images";
                     <p><?=$value["img_width"]?> x <?=$value["img_height"]?> pixels</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
                     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-                    <img src=./images/<?=$value["img_filename"]?> width='300' height='200'/></br>
+                    <img src=./images/<?=$value["img_filename"]?> width='300' height='225'/></br>
                     <a href="stad.php? img_id = <?=$value["img_id"]?>">Meer Info</a>
                 </div>
                 <?php
