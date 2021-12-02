a34f75377cf66b6a87ddb71accc2e932eeb2751a
Tim Vercammen <83355491+timvercammen@users.noreply.github.com>
2021-12-02 11:42:26 +0100

    oef2.1

--- config.php
+++ config.php
@@ -9,3 +9,13 @@
 
 global $servername,$username, $password, $dbname;
 ?>
+
+<?php
+
+$conn = new mysqli ($servername,$username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
--- steden2.php
+++ steden2.php
@@ -25,7 +25,6 @@
 
 <?php
 require_once "config.php";
-require_once "connection.php";
 require_once "getData.php";
 $sql = "SELECT * from images";
 ?>
